8fef3590d4bbe18dc73f6c6d3196766205190199
George Harries <george.harries@gmail.com>
2023-03-02 11:51:04 +0000

    Adding bevy and open basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
+pub const WINDOW_WIDTH: f32  = 1280.0;
+pub const WINDOW_HEIGHT: f32 =  720.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins.set(
+            WindowPlugin { 
+                window: WindowDescriptor{
+                    width: WINDOW_WIDTH,
+                    height: WINDOW_HEIGHT,
+                    title: "Bevy Pong Implementation".to_string(),
+                    resizable: false,
+                    ..Default::default()
+                },
+                ..default()
+            }
+        ))
+        .run();
 }
