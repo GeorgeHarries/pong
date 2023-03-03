1117cb221e3c8e6dbd3b7ba7d17e395d2fd73aaa
George Harries <george.harries@gmail.com>
2023-03-03 13:30:39 +0000

    Spawning Rackets

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
+
+pub const ASPECT_RATIO: f32 = 16.0/9.0;
 pub const WINDOW_HEIGHT: f32 =  720.0;
-pub const RESOLUTION: f32 = 16.0/9.0;
+pub const WINDOW_WIDTH: f32 = WINDOW_HEIGHT*ASPECT_RATIO;
+
+pub const RACKET_HEIGHT: f32 = 25.0;
+pub const RACKET_WIDTH: f32 = 100.0;
+pub const RACKET_EDGE_OFFSET: f32 = 50.0;
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Racket;
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Player {
+    player_number: u8
+}
 
 fn main() {
     App::new()
@@ -9,7 +26,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(
             WindowPlugin { 
                 window: WindowDescriptor{
-                    width: WINDOW_HEIGHT*RESOLUTION,
+                    width: WINDOW_WIDTH,
                     height: WINDOW_HEIGHT,
                     title: "Bevy Pong Implementation".to_string(),
                     resizable: false,
@@ -18,24 +35,45 @@ fn main() {
                 ..default()
             }
         ))
+        .add_plugin(WorldInspectorPlugin)
+        .register_type::<Racket>()
+        .register_type::<Player>()
         .add_startup_system(spawn_camera)
-        .add_startup_system(spawn_test_text)
+        .add_startup_system(spawn_rackets)
         .run();
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default()).insert(Name::new("Camera"));
 }
 
-fn spawn_test_text(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let text_style = TextStyle { 
-        font: asset_server.load("fonts/arial.ttf"), 
-        font_size: 60.0, 
-        color: Color::WHITE
-    };
-    commands.spawn(Text2dBundle {
-        text: Text::from_section("This is a test", text_style.clone())
-            .with_alignment(TextAlignment::CENTER),
+fn spawn_rackets(
+    mut commands: Commands,
+) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::WHITE,
+            custom_size: Some(Vec2::new(RACKET_HEIGHT, RACKET_WIDTH)),
+            ..default()
+        },
+        transform: Transform::from_xyz(-WINDOW_WIDTH/2.0+RACKET_EDGE_OFFSET, 0.0, 0.0),
         ..default()
-    });
+    })
+    .insert(Name::new("Player 1 Racket"))
+    .insert(Racket)
+    .insert(Player {player_number: 1});
+
+commands.spawn(SpriteBundle {
+    sprite: Sprite {
+            color: Color::WHITE,
+            custom_size: Some(Vec2::new(RACKET_HEIGHT, RACKET_WIDTH)),
+            ..default()
+        },
+        transform: Transform::from_xyz(WINDOW_WIDTH/2.0-RACKET_EDGE_OFFSET, 0.0, 0.0),
+        ..default()
+    })
+    .insert(Name::new("Player 2 Racket"))
+    .insert(Racket)
+    .insert(Player {player_number: 2});
 }
+
