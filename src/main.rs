9329956d79e00a8ab20e6ec08261ce81c7ebf2f9
George Harries <george.harries@gmail.com>
2023-03-03 16:12:32 +0000

    Added ball and basic reflection

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,23 @@ pub const ASPECT_RATIO: f32 = 16.0/9.0;
 pub const WINDOW_HEIGHT: f32 =  720.0;
 pub const WINDOW_WIDTH: f32 = WINDOW_HEIGHT*ASPECT_RATIO;
 
-pub const RACKET_HEIGHT: f32 = 25.0;
-pub const RACKET_WIDTH: f32 = 100.0;
+pub const RACKET_HEIGHT: f32 = 100.0;
+pub const RACKET_WIDTH: f32 = 25.0;
 pub const RACKET_EDGE_OFFSET: f32 = 50.0;
-
 pub const RACKET_SPEED: f32 = 10.0;
+
+pub const BALL_SIZE: f32 = 5.0;
+pub const BALL_SPEED: f32 = 10.0;
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
 pub struct Racket;
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Ball {
+    direction: f32
+}
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
@@ -39,10 +47,13 @@ fn main() {
         ))
         // .add_plugin(WorldInspectorPlugin)
         .register_type::<Racket>()
+        .register_type::<Ball>()
         .register_type::<Player>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_rackets)
+        .add_startup_system(spawn_ball)
         .add_system(control_rackets)
+        .add_system(move_balls)
         .run();
 }
 
@@ -50,13 +61,15 @@ fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default()).insert(Name::new("Camera"));
 }
 
+
+
 fn spawn_rackets(
     mut commands: Commands,
 ) {
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::WHITE,
-            custom_size: Some(Vec2::new(RACKET_HEIGHT, RACKET_WIDTH)),
+            custom_size: Some(Vec2::new(RACKET_WIDTH, RACKET_HEIGHT)),
             ..default()
         },
         transform: Transform::from_xyz(-0.5*WINDOW_WIDTH+RACKET_EDGE_OFFSET, 0.0, 0.0),
@@ -69,7 +82,7 @@ fn spawn_rackets(
 commands.spawn(SpriteBundle {
     sprite: Sprite {
             color: Color::WHITE,
-            custom_size: Some(Vec2::new(RACKET_HEIGHT, RACKET_WIDTH)),
+            custom_size: Some(Vec2::new(RACKET_WIDTH, RACKET_HEIGHT)),
             ..default()
         },
         transform: Transform::from_xyz(0.5*WINDOW_WIDTH-RACKET_EDGE_OFFSET, 0.0, 0.0),
@@ -114,5 +127,42 @@ fn control_rackets(
                 transform.translation.y -= RACKET_SPEED;
             }
         }
+    }
+}
+
+
+
+fn spawn_ball(
+    mut commands: Commands,
+) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::WHITE,
+            custom_size: Some(Vec2::new(BALL_SIZE, BALL_SIZE)),
+            ..default()
+        },
+        ..default()
+    })
+    .insert(Name::new("Ball"))
+    .insert(Ball {direction: 0.123*std::f32::consts::PI});
+}
+
+fn move_balls(
+    mut balls: Query<(&mut Ball, &mut Transform)>
+) {
+    for (mut ball, mut transform) in balls.iter_mut() {
+        // Wall reflect
+        if transform.translation.y.abs() >= 0.5*WINDOW_HEIGHT {
+            ball.direction = std::f32::consts::PI - ball.direction
+        }
+        
+        // Bounce at racket x pos TODO: Check racket y pos 
+        if transform.translation.x.abs() >= 0.5*WINDOW_WIDTH - RACKET_EDGE_OFFSET - 0.5*RACKET_WIDTH {
+            ball.direction = 2.0*std::f32::consts::PI - ball.direction;
+        }
+
+        // Move forward
+        transform.translation.x += (ball.direction.sin())*BALL_SPEED;
+        transform.translation.y += (ball.direction.cos())*BALL_SPEED;
     }
 }
